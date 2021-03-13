eeae9be15639bbf99572f88575b2673d00b81aba
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2021-03-13 20:12:38 +0200

    HW001

---
+++ HW001/Task01.java
@@ -0,0 +1,21 @@
+package HW001;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		int a = 19;
+		long b = 20_125_154;
+		byte c = 127;
+		short e = 15;
+		char ch = 'c';
+		boolean t = true;
+		float y = 1.4f;
+		double k = 1.8;
+
+		System.out.println(a + "\n" + b + "\n" + 
+				c + "\n" + e + "\n" + 
+				ch + "\n" + t + "\n" + 
+				y + "\n" + k);
+	}
+
+}
---
+++ HW001/Task02.java
@@ -0,0 +1,10 @@
+package HW001;
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
