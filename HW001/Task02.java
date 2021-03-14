b2110478cbed67040e93abd7d1d4602c6460c469
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2021-03-14 12:22:48 +0200

    Task02

--- HW001/Task02.java
+++ HW001/Task02.java
@@ -3,8 +3,19 @@ package HW001;
 public class Task02 {
 
 	public static void main(String[] args) {
-		
+		long longValue = 50;
+		int intValue = 29;
+		byte byteValue = 89;
+		short shortValue = 138;
+		float floatValue = 134.34f;
+		double doubleValue = 345.56;
 
+		int result1 = byteValue + shortValue;
+		long result2 = longValue + intValue;
+		double result3 = floatValue + doubleValue;
+		double result4 = intValue + doubleValue;
+
+		System.out.println(result1 + "\n" + result2 + "\n" + 
+						   result3 + "\n" + result4);
 	}
-
 }
